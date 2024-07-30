69ec266fb816e689441d53de5d347df2aed6b006
WillianGrecco <124214097+WillianGrecco@users.noreply.github.com>
2024-07-30 19:37:48 -0300

    Add files via upload

--- Para Casa 114/test2.py
+++ Para Casa 114/test2.py
@@ -1,14 +1,8 @@
-a=33
-b="33"
-
-if b > a:
-    print("b é maior que a")
-elif b == a:
-    print("a e b são iguais")
-
 def my_function(fname):
     print(fname + "Refsnes")
 
 my_function("Emilly")
 my_function("Tobias")
 my_function("Lucas")
+
+#Os nomes ficaram com "Refsnes" na frebte deles
